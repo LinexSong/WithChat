121690216fc34324a4523671422cf5a156041467
LinexSong <linexwolf@icloud.com>
2024-08-26 14:51:21 +0800

    fix: 状态码现在以整型返回前端

--- WithChatBackEnd/chat-common/src/main/java/com/linex/enums/Status.java
+++ WithChatBackEnd/chat-common/src/main/java/com/linex/enums/Status.java
@@ -1,6 +1,7 @@
 package com.linex.enums;
 
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 
 /**
@@ -12,10 +13,10 @@ public enum Status {
      * 0 - 成功
      */
     SUCCESS(0, "success"),
-    ERROR(1, "fail")
-    ;
-    private Integer code;
-    private String desc;
+    ERROR(1, "fail");
+    @JsonValue
+    private final Integer code;
+    private final String desc;
 
     Status(Integer code, String desc) {
         this.code = code;
